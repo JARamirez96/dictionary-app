9cc36674cf1c635a9b947d2a73cc01a0a5f08940
JARamirez96 <josuealexis96@gmail.com>
2024-07-18 16:58:15 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const handleSearchInput = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center my-6 text-4xl">Dictionary App</h1>
+      <form>
+        <div className="w-6/12 m-auto flex">
+          <input
+            id="search"
+            type="text"
+            placeholder="Enter word"
+            className="w-full rounded-s-lg bg-gray-50 border border-gray-300 p-2.5"
+          />
+          <button
+            onClick={handleSearchInput}
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium py-2 px-5 rounded-e-lg text-lg"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
